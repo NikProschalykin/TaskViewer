04cb2083293b0446969e3ce67a930518bd499293
Nikolay Proschalykin <n.proschalykin@mail.ru>
2023-09-12 19:06:15 +0300

    initial commit

---
+++ TaskViewer/TaskViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskViewerApp.swift
+//  TaskViewer
+//
+//  Created by Николай Прощалыкин on 12.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
